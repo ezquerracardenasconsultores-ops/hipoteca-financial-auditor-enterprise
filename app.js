2cf517348efd34c4119d442dd942a69818f71e39
ezquerracardenasconsultores-ops <ezquerracardenasconsultores@gmail.com>
2026-07-08 11:10:55 -0500

    Add function to generate amortization schedule

--- app.js
+++ app.js
@@ -65,3 +65,67 @@ function calcular() {
         del cronograma y la auditoría financiera.
     `;
 }
+function generarCronograma(capital, tea, plazo){
+
+    const tem = Math.pow(1 + tea / 100, 1 / 12) - 1;
+
+    const cuota =
+        capital *
+        ((tem * Math.pow(1 + tem, plazo)) /
+        (Math.pow(1 + tem, plazo) - 1));
+
+    let saldo = capital;
+
+    let html = `
+    <table class="tabla">
+
+        <tr>
+
+            <th>Cuota</th>
+
+            <th>Interés</th>
+
+            <th>Amortización</th>
+
+            <th>Saldo</th>
+
+        </tr>
+    `;
+
+    for(let i=1;i<=plazo;i++){
+
+        let interes = saldo * tem;
+
+        let amortizacion = cuota - interes;
+
+        saldo -= amortizacion;
+
+        if(saldo<0){
+
+            saldo=0;
+
+        }
+
+        html += `
+
+        <tr>
+
+            <td>${i}</td>
+
+            <td>S/ ${interes.toLocaleString("es-PE",{minimumFractionDigits:2})}</td>
+
+            <td>S/ ${amortizacion.toLocaleString("es-PE",{minimumFractionDigits:2})}</td>
+
+            <td>S/ ${saldo.toLocaleString("es-PE",{minimumFractionDigits:2})}</td>
+
+        </tr>
+
+        `;
+
+    }
+
+    html += "</table>";
+
+    document.getElementById("cronograma").innerHTML = html;
+
+}
